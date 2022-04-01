d84497da157a16ff95c15af13ac7ad4bd5630fcf
Aaron Erlandson <wl7521ol@go.minnstate.edu>
2022-04-01 17:47:08 -0500

    output states graph and my poster work

--- usa_cwd.R
+++ usa_cwd.R
@@ -34,11 +34,24 @@ ggplot(data = usa_cwd,
 
 
 # Bar chart showing state vs total_positive_samples vs states
-ggplot(data = usa_cwd,
-       mapping = aes(x = state)) +
+usa_cwd %>%
+  group_by(state) %>% 
+  summarise(total_positive_samples = sum(total_positive_samples)) %>% 
+  arrange(desc(total_positive_samples)) %>% 
+  mutate(state = as_factor(state)) %>% 
+  ggplot(mapping = aes(x = state)) +
   geom_col(mapping = aes(y = total_positive_samples), fill = "#a6192e") +
-  theme(axis.text.x = element_text(angle = 90, size = 10))
+  theme_gray(base_size =24 )+
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(y = "Total Number of Positive Samples", x = NULL)
+ggsave(filename = "output/states graph.png",width = 14, height = 7,
+       units = "in", dpi = 400)
+
+
 
 # Calculating Positivity sample
-ps <- select(usa_cwd, total_collected_samples, total_positive_samples)
-mutate(ps, positivity_sample = total_positive_samples / total_collected_samples)
+ps <- select(usa_cwd, total_collected_samples, total_positive_samples, state, year, species)
+ps <- mutate(ps, positivity_sample = total_positive_samples / total_collected_samples)
+
+
+Calculating 
